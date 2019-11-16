52b0cfba2d66b53107c33f5b6b8f957306240f3a
David <DMcCulloch86@gmail.com>
2019-11-16 17:26:12 -0500

    concert-this and do-what-it-says now function properly, there was a bug of a missing .data in all the output for concert-this

--- liri.js
+++ liri.js
@@ -1,13 +1,15 @@
 const axios = require('axios');
 const moment = require('moment');
+const Spotify = require('node-spotify-api');
 require("dotenv").config();
 
 const keys = require('./keys.js');
 const fs = require('fs')
 
-// let spotify = new Spotify(keys.spotify);
+let spotify = new Spotify(keys.spotify);
 
 let command = process.argv[2]
+let commandData = process.argv[3]
 
 function logInput () {
     let data = process.argv.splice(2);
@@ -21,15 +23,20 @@ function logInput () {
 }
 
 switch (command) {
+    case "do-what-it-says":
+        let dataBuffer = fs.readFileSync('random.txt');
+        let parse = dataBuffer.toString();
+        let data = parse.split(',');
+        command = data[0];
+        commandData = data[1];
     case "concert-this":
-        let artist = process.argv[3].toLowerCase().split(' ').join('+')
-        console.log('test-artist' + artist)
+        let artist = commandData.toLowerCase().split(' ').join('+')
         axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
         .then(function(response) {
-            console.log(`Artist: ${response[0].artist.name}`);
-            console.log(`Venue: ${response[0].venue.name}`)
-            console.log(`Venue Location: ${response[0].venue.city}, ${response[0].venue.region}`)
-            let eventTime = response[0].venue.datetime
+            console.log(`Artist: ${response.data[0].artist.name}`);
+            console.log(`Venue: ${response.data[0].venue.name}`)
+            console.log(`Venue Location: ${response.data[0].venue.city}, ${response.data[0].venue.region}`)
+            let eventTime = response.data[0].venue.datetime
             let eventTimeVisual = moment(eventTime).format("MM/DD/YYYY");
             console.log(`Event Date: ${eventTimeVisual}`)
         })
@@ -38,15 +45,22 @@ switch (command) {
         })
         break;
     case "spotify-this-song":
-        let artist = process.argv[3].toLowerCase().split(' ').join('+')
-        console.log("spotify-this-song")
+        if (commandData) {
+            let song = commandData.toLowerCase().split(' ').join('+')
+        } else {
+            let song = "the+sign"
+        }
+        spotify.search({ type: 'track', query: song}, function (err, data) {
+            if (err) {
+                return console.log(`Spotify Error: ${err}`)
+            }
+            console.log(data);
+        })
         break;
     case "movie-this":
         console.log("movie-this")
         break;
-    case "do-what-it-says":
-        console.log("do-what-it-says")
-        break;
+    
 }
 
 logInput();
